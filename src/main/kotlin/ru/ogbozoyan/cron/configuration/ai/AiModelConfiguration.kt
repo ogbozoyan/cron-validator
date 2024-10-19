c62c41f8efbb606088e542cb24a8c122ad849cac
ogbozoyan <oganes.bozoyan@mail.ru>
2024-10-20 02:28:46 +0300

    fix
    Signed-off-by: ogbozoyan <oganes.bozoyan@mail.ru>

--- src/main/kotlin/ru/ogbozoyan/cron/configuration/ai/AiModelConfiguration.kt
+++ src/main/kotlin/ru/ogbozoyan/cron/configuration/ai/AiModelConfiguration.kt
@@ -8,6 +8,9 @@ import org.springframework.ai.chat.memory.ChatMemory
 import org.springframework.ai.chat.memory.InMemoryChatMemory
 import org.springframework.ai.embedding.EmbeddingModel
 import org.springframework.ai.evaluation.FactCheckingEvaluator
+import org.springframework.ai.ollama.OllamaChatModel
+import org.springframework.ai.ollama.api.OllamaApi
+import org.springframework.ai.ollama.api.OllamaOptions
 import org.springframework.ai.vectorstore.SimpleVectorStore
 import org.springframework.ai.vectorstore.VectorStore
 import org.springframework.beans.factory.annotation.Value
@@ -18,12 +21,15 @@ import org.springframework.core.io.Resource
 @Configuration
 class AiModelConfiguration(
     private val chatClientBuilder: ChatClient.Builder,
-    @Value("classpath:/prompts/system-message.st") private val systemMessage: Resource
+    @Value("\${spring.ai.ollama.chat.options.model}") private val model: String,
+    @Value("classpath:/prompts/system-message.st") private val systemMessage: Resource,
+//    @Value("classpath:/prompts/prompt-checking-message.st") private val safetyPrompt: Resource,
 ) {
 
     fun chatMemory(): ChatMemory {
         return InMemoryChatMemory()
     }
+
 
     @Bean
     fun ollamaClient(): ChatClient {
@@ -31,6 +37,7 @@ class AiModelConfiguration(
             .defaultSystem(systemMessage)
             .defaultAdvisors(
                 MessageChatMemoryAdvisor(chatMemory(), DEFAULT_CHAT_MEMORY_CONVERSATION_ID, 5),
+//                PromptSafeCheckAdvisor(promptSafetyClient(), safetyPrompt),
                 SimpleLoggerAdvisor()
             )
             .build()
@@ -44,9 +51,23 @@ class AiModelConfiguration(
         return FactCheckingEvaluator(chatClientBuilder)
     }
 
+
     @Bean
     fun simpleVectorStore(embeddingModel: EmbeddingModel): VectorStore {
         return SimpleVectorStore(embeddingModel)
     }
 
+    fun promptSafetyClient(): ChatClient = ChatClient.builder(
+        OllamaChatModel(
+            OllamaApi(), OllamaOptions
+                .create()
+                .withModel(model)
+                .withTemperature(0.4)
+                .build()
+        )
+    )
+        .defaultAdvisors(
+            SimpleLoggerAdvisor()
+        )
+        .build()
 }
