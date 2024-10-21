b31a64b24e6147b74aa898867fdfef97b4df3d40
ogbozoyan <ogbozoyan@mail.ru>
2024-10-21 15:14:57 +0300

    misc changes

--- src/main/kotlin/ru/ogbozoyan/cron/configuration/ai/AiModelConfiguration.kt
+++ src/main/kotlin/ru/ogbozoyan/cron/configuration/ai/AiModelConfiguration.kt
@@ -2,8 +2,9 @@ package ru.ogbozoyan.cron.configuration.ai
 
 import org.springframework.ai.chat.client.ChatClient
 import org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.DEFAULT_CHAT_MEMORY_CONVERSATION_ID
-import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor
+import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor
+import org.springframework.ai.chat.client.advisor.VectorStoreChatMemoryAdvisor
 import org.springframework.ai.chat.memory.ChatMemory
 import org.springframework.ai.chat.memory.InMemoryChatMemory
 import org.springframework.ai.embedding.EmbeddingModel
@@ -11,6 +12,7 @@ import org.springframework.ai.evaluation.FactCheckingEvaluator
 import org.springframework.ai.ollama.OllamaChatModel
 import org.springframework.ai.ollama.api.OllamaApi
 import org.springframework.ai.ollama.api.OllamaOptions
+import org.springframework.ai.vectorstore.SearchRequest
 import org.springframework.ai.vectorstore.SimpleVectorStore
 import org.springframework.ai.vectorstore.VectorStore
 import org.springframework.beans.factory.annotation.Value
@@ -18,25 +20,23 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.io.Resource
 
+
 @Configuration
 class AiModelConfiguration(
     private val chatClientBuilder: ChatClient.Builder,
     @Value("\${spring.ai.ollama.chat.options.model}") private val model: String,
     @Value("classpath:/prompts/system-message.st") private val systemMessage: Resource,
+    private val vectorStore: VectorStore
 //    @Value("classpath:/prompts/prompt-checking-message.st") private val safetyPrompt: Resource,
 ) {
-
-    fun chatMemory(): ChatMemory {
-        return InMemoryChatMemory()
-    }
-
 
     @Bean
     fun ollamaClient(): ChatClient {
         return chatClientBuilder
             .defaultSystem(systemMessage)
             .defaultAdvisors(
-                MessageChatMemoryAdvisor(chatMemory(), DEFAULT_CHAT_MEMORY_CONVERSATION_ID, 5),
+                VectorStoreChatMemoryAdvisor(vectorStore, DEFAULT_CHAT_MEMORY_CONVERSATION_ID, 5),
+                QuestionAnswerAdvisor(vectorStore, SearchRequest.defaults()), // RAG advisor
 //                PromptSafeCheckAdvisor(promptSafetyClient(), safetyPrompt),
                 SimpleLoggerAdvisor()
             )
@@ -54,6 +54,11 @@ class AiModelConfiguration(
     @Bean
     fun simpleVectorStore(embeddingModel: EmbeddingModel): VectorStore {
         return SimpleVectorStore(embeddingModel)
+    }
+
+    @Bean
+    fun inMemoryChatMemory(): ChatMemory {
+        return InMemoryChatMemory()
     }
 
     fun promptSafetyClient(): ChatClient = ChatClient.builder(
--- src/main/kotlin/ru/ogbozoyan/cron/configuration/ai/PromptSafeCheckAdvisor.kt
+++ src/main/kotlin/ru/ogbozoyan/cron/configuration/ai/PromptSafeCheckAdvisor.kt
@@ -1,9 +1,11 @@
 package ru.ogbozoyan.cron.configuration.ai
 
+import org.slf4j.LoggerFactory
 import org.springframework.ai.chat.client.ChatClient
 import org.springframework.ai.chat.client.advisor.api.*
 import org.springframework.ai.chat.model.ChatResponse
 import org.springframework.ai.chat.prompt.PromptTemplate
+import org.springframework.core.Ordered
 import org.springframework.core.io.Resource
 import reactor.core.publisher.Flux
 
@@ -15,49 +17,56 @@ class PromptSafeCheckAdvisor(
     CallAroundAdvisor,
     StreamAroundAdvisor {
 
+    private val log = LoggerFactory.getLogger(PromptSafeCheckAdvisor::class.java)
+
     private val INJECTION = "YES"
 
-
     override fun getOrder(): Int {
-        return 0
+        return Ordered.HIGHEST_PRECEDENCE
     }
-
 
     override fun getName(): String {
         return javaClass.simpleName
     }
 
-
     override fun aroundStream(
         advisedRequest: AdvisedRequest,
         chain: StreamAroundAdvisorChain
     ): Flux<AdvisedResponse> {
-        val promptTemplate = PromptTemplate(prompt)
-        val promptParameters = HashMap<String, Any>()
-        promptParameters["input"] = advisedRequest.userText
+        try {
+            val promptTemplate = PromptTemplate(prompt)
+            val promptParameters = HashMap<String, Any>()
+            promptParameters["input"] = advisedRequest.userText
 
-        val responseSpec = promptSafetyClient.prompt(promptTemplate.create(promptParameters))
-            .call()
-        val content = responseSpec.content()
+            val responseSpec = promptSafetyClient.prompt(promptTemplate.create(promptParameters))
+                .call()
+            val content = responseSpec.content()
 
-        if (content == null || content.isEmpty() || content.lowercase() == INJECTION) {
-            return Flux.just(createFailureResponse(responseSpec.chatResponse(), advisedRequest))
+            if (content == null || content.isEmpty() || content.lowercase() == INJECTION) {
+                return Flux.just(createFailureResponse(responseSpec.chatResponse(), advisedRequest))
+            }
+        } catch (e: Throwable) {
+            log.error("Error while processing aroundStream in {}", name, e)
         }
 
         return chain.nextAroundStream(advisedRequest)
     }
 
     override fun aroundCall(advisedRequest: AdvisedRequest, chain: CallAroundAdvisorChain): AdvisedResponse {
-        val promptTemplate = PromptTemplate(prompt)
-        val promptParameters = HashMap<String, Any>()
-        promptParameters["input"] = advisedRequest.userText
+        try {
+            val promptTemplate = PromptTemplate(prompt)
+            val promptParameters = HashMap<String, Any>()
+            promptParameters["input"] = advisedRequest.userText
 
-        val responseSpec = promptSafetyClient.prompt(promptTemplate.create(promptParameters))
-            .call()
-        val content = responseSpec.content()
+            val responseSpec = promptSafetyClient.prompt(promptTemplate.create(promptParameters))
+                .call()
+            val content = responseSpec.content()
 
-        if (content == null || content.isEmpty() || content.uppercase() == INJECTION) {
-            return createFailureResponse(responseSpec.chatResponse(), advisedRequest)
+            if (content == null || content.isEmpty() || content.uppercase() == INJECTION) {
+                return createFailureResponse(responseSpec.chatResponse(), advisedRequest)
+            }
+        } catch (e: Exception) {
+            log.error("Error while processing aroundCall in {}", name, e)
         }
 
         return chain.nextAroundCall(advisedRequest)
