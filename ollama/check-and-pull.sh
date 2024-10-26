9616d7caaea32a1a554a7e86fc78672e87f03491
ogbozoyan <oganes.bozoyan@mail.ru>
2024-10-26 14:22:37 +0300

    micro fix
    Signed-off-by: ogbozoyan <oganes.bozoyan@mail.ru>

--- ollama/check-and-pull.sh
+++ ollama/check-and-pull.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # Check if mistral is in the list of models
-if ollama list | grep -q 'mistral'; then
+if ollama list | grep 'mistral'; then
   echo "mistral is already installed."
 else
   echo "mistral not found. Pulling the model..."
   ollama pull mistral
 fi
 
-if ollama list | grep -q 'mxbai-embed-large'; then
+if ollama list | grep 'mxbai-embed-large'; then
   echo "mxbai-embed-large: is already installed."
 else
   echo "mxbai-embed-large: not found. Pulling the model..."
